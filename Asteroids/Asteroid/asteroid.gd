54da6cc15ad280b8fdb253cda3d5ca814c1848e6
Cold-Change <jaredsimonetti2@gmail.com>
2024-09-30 19:29:49 -0400

    Update: Asteroids
    Improved player movement
    Added asteroid splitting

--- Asteroids/Asteroid/asteroid.gd
+++ Asteroids/Asteroid/asteroid.gd
@@ -1,7 +1,45 @@
 extends RigidBody2D
+class_name Asteroid
 
-#func _ready():
-	#linear_velocity = Vector2(200,100)
+@onready var size = 5
+@onready var splits = 4
+@onready var speed : Vector2
+
+func _ready():
+	initMovement()
 
 func _physics_process(delta):
-	move_and_collide(Vector2(2,1))
+	move_and_collide(speed*delta)
+
+func breakAsteroid():
+	if splits:
+		var new_asteroid_1 = load("res://Asteroid/asteroid.tscn").instantiate()
+		get_parent().add_child(new_asteroid_1)
+		new_asteroid_1.size = size - 1
+		new_asteroid_1.splits = splits - 1
+		new_asteroid_1.position = position
+		new_asteroid_1.initMovement()
+		var new_asteroid_2 = load("res://Asteroid/asteroid.tscn").instantiate()
+		get_parent().add_child(new_asteroid_2)
+		new_asteroid_2.size = size - 1
+		new_asteroid_2.splits = splits - 1
+		new_asteroid_2.position = position
+		new_asteroid_2.initMovement()
+	queue_free()
+
+func initMovement():
+	var angle = randf_range(-PI,PI)
+	if size == 5:
+		speed = Vector2(randi_range(200,250)*sin(angle),randi_range(200,250)*-cos(angle))
+	elif size == 4:
+		speed = Vector2(randi_range(200,250)*sin(angle),randi_range(200,250)*-cos(angle))
+		scale *= .75
+	elif size == 3:
+		speed = Vector2(randi_range(300,350)*sin(angle),randi_range(300,350)*-cos(angle))
+		scale *= .5
+	elif size == 2:
+		speed = Vector2(randi_range(400,450)*sin(angle),randi_range(400,450)*-cos(angle))
+		scale *= .25
+	elif size == 1:
+		speed = Vector2(randi_range(500,550)*sin(angle),randi_range(500,550)*-cos(angle))
+		scale *= .15
--- Asteroids/Player/player.gd
+++ Asteroids/Player/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @export var speed = 250
-@export var acceleration = 600
+@export var acceleration = 400
 @onready var init_acceleration = acceleration
+@onready var friction = 100
+@onready var init_friction = friction
 
 @onready var area_2d = $Area2D
 @onready var invulnerability_timer = $Timers/Invulnerability
@@ -17,20 +19,28 @@ func _physics_process(delta):
 func handleMovement(delta):
 	var turn = Input.get_axis("left","right")
 	if Input.is_action_pressed("propulsion"):
-		if velocity.x + sin(rotation)*acceleration*delta > speed:
-			velocity.x = speed
-		elif velocity.x + sin(rotation)*acceleration*delta < -speed:
-			velocity.x = -speed
-		else:
-			velocity.x += sin(rotation)*acceleration*delta
-		if velocity.y + -cos(rotation)*acceleration*delta > speed:
-			velocity.y = speed
-		elif velocity.y + -cos(rotation)*acceleration*delta < -speed:
-			velocity.y = -speed
-		else:
-			velocity.y += -cos(rotation)*acceleration*delta
+		applyAcceleration(delta)
+	applyFriction(delta)
 	if turn:
 		rotate(PI*turn*delta)
+
+func applyAcceleration(delta):
+	if velocity.x + sin(rotation)*acceleration*delta > speed:
+		velocity.x = speed
+	elif velocity.x + sin(rotation)*acceleration*delta < -speed:
+		velocity.x = -speed
+	else:
+		velocity.x += sin(rotation)*acceleration*delta
+	if velocity.y + -cos(rotation)*acceleration*delta > speed:
+		velocity.y = speed
+	elif velocity.y + -cos(rotation)*acceleration*delta < -speed:
+		velocity.y = -speed
+	else:
+		velocity.y += -cos(rotation)*acceleration*delta
+
+func applyFriction(delta):
+	velocity.x = move_toward(velocity.x,0,friction*delta)
+	velocity.y = move_toward(velocity.y,0,friction*delta)
 
 func handleInvulnerability():
 	if invulnerability_timer.is_stopped():
@@ -40,16 +50,17 @@ func handleInvulnerability():
 			visible = false
 		else:
 			visible = true
-	
 
 func _on_area_2d_body_entered(body):
-	if body.has_method("break"):
-		body.break()
+	if body.has_method("breakAsteroid"):
+		body.call_deferred("breakAsteroid")
 	receiveDamage.emit()
 	area_2d.set_deferred("monitoring",false)
 	acceleration = 100
+	friction = 50
 	invulnerability_timer.start()
 
 func _on_invulnerability_timeout():
 	area_2d.set_deferred("monitoring",true)
 	acceleration = init_acceleration
+	friction = init_friction
