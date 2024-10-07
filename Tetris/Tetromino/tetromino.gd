570392947b7b7dfe847e75f56282e6620a579c2c
Cold-Change <jaredsimonetti2@gmail.com>
2024-10-07 00:13:19 -0400

    Update: Tetris
    Added tetris block instantiation

---
+++ Tetris/Tetromino/tetromino.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var tetromino_type : String
+
+var cells = Globals.cells
+var tetromino = Globals.Tetromino
+
+func _ready():
+	for i in cells[tetromino[tetromino_type]]:
+		var new_square = load("res://Tetromino/tetromino_square.tscn").instantiate()
+		if i != Vector2.ZERO:
+			add_child(new_square)
+			new_square.position.x += i.x * 32
+			new_square.position.y += i.y * 32
+			new_square.name = "TetrominoSquare"
+	for child in get_children():
+		child.get_node("TetrominoSquares-sheet").set_frame(tetromino[tetromino_type])
