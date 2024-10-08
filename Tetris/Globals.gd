1d7c0aa04ea9b3b2a3de47d423c06ef9f3fe29c6
Cold-Change <jaredsimonetti2@gmail.com>
2024-10-07 20:53:42 -0400

    Update: Tetris
    Updated graphics
    Implemented tetromino movement restrictions

--- Tetris/Globals.gd
+++ Tetris/Globals.gd
@@ -5,10 +5,10 @@ enum Tetromino {
 }
 
 var cells = {
-	Tetromino.I: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
+	Tetromino.I: [Vector2(-2, 0), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
 	Tetromino.J: [Vector2(-1, 1), Vector2(-1, 0), Vector2(0,0), Vector2(1, 0 )],
 	Tetromino.L: [Vector2(1,1), Vector2(-1, 0), Vector2(0,0), Vector2(1,0)],
-	Tetromino.O: [Vector2(0,1), Vector2(1,1), Vector2(0,0), Vector2(1,0)],
+	Tetromino.O: [Vector2(-1,0), Vector2(-1,1), Vector2(0,0), Vector2(0,1)],
 	Tetromino.S: [Vector2(-1, 1), Vector2(0, 1), Vector2(0,0), Vector2(1, 0)],
 	Tetromino.T: [Vector2(0,1), Vector2(-1, 0), Vector2(0,0), Vector2(1,0)],
 	Tetromino.Z: [Vector2(0,1), Vector2(1,1), Vector2(-1, 0), Vector2(0,0)]
--- Tetris/Tetris Game/tetris_game.gd
+++ Tetris/Tetris Game/tetris_game.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @onready var movement_timer = $MovementTimer
 
-var window_width = ProjectSettings.get_setting("display/window/size/viewport_width")
-var window_height = ProjectSettings.get_setting("display/window/size/viewport_height")
+var board_width = 320
+var board_height = 640
 
 var tetromino = preload("res://Tetromino/tetromino.tscn")
 
@@ -14,18 +14,46 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("accept"):
 		var new_tetromino = tetromino.instantiate()
 		add_child(new_tetromino)
-		new_tetromino.position = Vector2(window_width/2,32)
+		new_tetromino.position = Vector2(board_width/2,32)
 		new_tetromino.initTetromino(Globals.Tetromino.keys()[randi() % Globals.Tetromino.size()])
 		movement_timer.start()
 	if has_node("Tetromino"):
 		if Input.is_action_just_pressed("up"):
-			get_node("Tetromino").rotation += PI/2
-			for square in get_node("Tetromino").get_children():
-				square.rotation -= PI/2
-		if Input.is_action_just_pressed("right"):
+			if floor(get_node("Tetromino").rotation) == 4:
+				get_node("Tetromino").rotation = 0
+				for square in get_node("Tetromino").get_children():
+					square.rotation = 0
+			else:
+				get_node("Tetromino").rotation += PI/2
+				for square in get_node("Tetromino").get_children():
+					square.rotation -= PI/2
+		if Input.is_action_just_pressed("right") and checkPositionOnMove("right"):
 			get_node("Tetromino").position.x += 32
-		if Input.is_action_just_pressed("left"):
+		if Input.is_action_just_pressed("left") and checkPositionOnMove("left"):
 			get_node("Tetromino").position.x -= 32
+
+func checkPositionOnMove(direction):
+	for square in get_node("Tetromino").get_children():
+		print(get_node("Tetromino").position.x + square.position.x)
+		print(get_node("Tetromino").rotation)
+		if direction == "right":
+			if floor(get_node("Tetromino").rotation) == 1:
+				if get_node("Tetromino").position.x - square.position.y + 32 > board_width:
+					return false
+			elif floor(get_node("Tetromino").rotation) == 4:
+				if get_node("Tetromino").position.x + square.position.y + 32 > board_width:
+					return false
+			elif floor(get_node("Tetromino").rotation) == 3:
+				if get_node("Tetromino").position.x + square.position.x > board_width:
+					return false
+			else:
+				if get_node("Tetromino").position.x + square.position.x + 32 > board_width:
+					return false
+		else:
+			if get_node("Tetromino").position.x + square.position.x - 32 < 0:
+				return false
+	return true
+
 func _on_movement_timer_timeout():
 	if has_node("Tetromino"):
 		get_node("Tetromino").position.y += 32
