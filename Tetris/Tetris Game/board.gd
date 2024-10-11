2516cf9ce3a08ee019d9576e16ac8ed16a1d95ec
Cold-Change <jaredsimonetti2@gmail.com>
2024-10-10 22:31:07 -0400

    Update: Tetris
    Final major update
    Fully working model of the game
    Game over condition can use some work as well as minor graphical bug when completing a line

--- Tetris/Tetris Game/board.gd
+++ Tetris/Tetris Game/board.gd
@@ -2,19 +2,31 @@ extends Node
 
 var board_array : Array[String] = []
 
+signal rowCleared
+signal increaseScore(amount)
+signal gameOver
+
 func _ready():
 	board_array.resize(240)
 	board_array.fill('')
 
+#Inputs tetromino type into board array according to the array of positions provided
 func lockTetrominoToBoard(tetromino_array,tetromino_type):
 	for i in tetromino_array:
-		board_array[i] = tetromino_type
+		if board_array[i]:
+			board_array[i-10] = tetromino_type
+		else:
+			board_array[i] = tetromino_type
 	updateBoard()
 
+#Displays un-displayed squares, clears empty spaces
 func updateBoard():
 	for square in get_children():
-		if !board_array[int(square.name.get_slice('Square',1))]:
+		var square_index = int(square.name.get_slice('Square',1))
+		if !board_array[square_index]:
 			square.free()
+		elif board_array[square_index] != Globals.Tetromino.find_key(square_index):
+			square.get_node("TetrominoSquares-sheet").set_frame(Globals.Tetromino[board_array[int(square.name.get_slice('Square',1))]])
 	for i in board_array.size():
 		if board_array[i] and !has_node("Square" + str(i)):
 			var x = i % 10
@@ -24,9 +36,38 @@ func updateBoard():
 			new_square.position = Vector2(x + .5,y - 3.5) * 32
 			new_square.name = "Square" + str(i)
 			new_square.get_node("TetrominoSquares-sheet").set_frame(Globals.Tetromino[board_array[i]])
+	checkForFullRow()
 
+#Provided and array of array location, checks for overlap between the array and the board array
 func checkForOverlap(tetromino_array):
 	for i in tetromino_array:
 		if board_array[i]:
 			return false #There is overlap
 	return true #There is no overlap
+
+func checkForGameOver():
+	for i in range(40):
+		if board_array[i]:
+			gameOver.emit()
+
+func checkForFullRow():
+	for row in range(24):
+		var row_is_full = true
+		for i in range(10):
+			if !board_array[row*10 + i]:
+				row_is_full = false
+		if row_is_full:
+			clearRow(row)
+
+func clearRow(row):
+	rowCleared.emit()
+	while row > 0:
+		for i in range(10):
+			board_array[row*10 + i] = board_array[row*10 + i - 10]
+		row -= 1
+	increaseScore.emit(100)
+	updateBoard()
+
+func clearBoard():
+	board_array.fill('')
+	updateBoard()
--- Tetris/Tetris Game/tetris_game.gd
+++ Tetris/Tetris Game/tetris_game.gd
@@ -1,20 +1,73 @@
 extends Node2D
 
 @onready var movement_timer = $MovementTimer
+@onready var cleanup_timer = $CleanupTimer
+
+@onready var start_ui = $Start
+@onready var game_over_ui = $GameOver
+@onready var score_label_1 = $GameOver/Panel/VBoxContainer/Score
+@onready var high_score_label_1 = $GameOver/Panel/VBoxContainer/HighScore
+@onready var running_ui = $Running
+@onready var hgih_score_label_2 = $Running/VBoxContainer/HgihScore
+@onready var score_label_2 = $Running/VBoxContainer/Score
 
 @onready var board = $Board
 var board_width = 320
 var board_height = 640
 
+@onready var cleanup = $Cleanup
+
 var tetromino = preload("res://Tetromino/tetromino.tscn")
 
+var score = 0
+var state = "Start"
+
 func _ready():
-	movement_timer.start()
-	createNewTetromino()
+	board.rowCleared.connect(initCleanup)
+	board.gameOver.connect(gameOver)
+	board.increaseScore.connect(increaseScore)
 
 func _physics_process(delta):
-	if has_node("Tetromino"):
+	if state == "Start" and Input.is_action_just_pressed("accept"):
+		startGame()
+	if state == "GameOver" and Input.is_action_just_pressed("accept"):
+		resetGame()
+	if state == "Running" and has_node("Tetromino"):
 		manageTetrominoMovement()
+		
+func startGame():
+	movement_timer.start()
+	createNewTetromino()
+	state = "Running"
+	hgih_score_label_2.text = "High Score: " + str(Globals.high_score)
+	score_label_2.text = "Score: " + str(score)
+	start_ui.visible = false
+	running_ui.visible = true
+
+func gameOver():
+	state = "GameOver"
+	movement_timer.stop()
+	if score > Globals.high_score:
+		Globals.high_score = score
+	score_label_1.text = "Your score is " + str(score)
+	high_score_label_1.text = "The high score is " + str(Globals.high_score)
+	running_ui.visible = false
+	game_over_ui.visible = true
+
+func resetGame():
+	state = "Start"
+	board.clearBoard()
+	get_node("Tetromino").queue_free()
+	score = 0
+	game_over_ui.visible = false
+	start_ui.visible = true
+
+func increaseScore(amount):
+	score += amount
+	score_label_2.text = "Score: " + str(score)
+	if score > Globals.high_score:
+		Globals.high_score = score
+		hgih_score_label_2.text = "High Score: " + str(Globals.high_score)
 
 func checkPositionOnMove(direction):
 	for square in get_node("Tetromino").get_children():
@@ -46,6 +99,7 @@ func checkPositionOnMove(direction):
 					return false
 	return true
 
+#Checks position and rotation of tetromino and returns a direction if the tetromino goes beyond the scope of the board
 func checkForWallKick():
 	for square in get_node("Tetromino").get_children():
 		if floor(get_node("Tetromino").rotation) == 1:
@@ -70,6 +124,8 @@ func checkForWallKick():
 				return 'right'
 	return ''
 
+#Shifts the tetromino left or right according to the direction provided
+#Recurs itself if a direction is provided
 func kickOffWall(direction):
 	if direction == 'left':
 		get_node("Tetromino").position.x -= 32
@@ -109,44 +165,53 @@ func manageTetrominoMovement():
 	if Input.is_action_just_pressed("down"):
 		shiftTetrominoDown()
 		movement_timer.start()
-	if Input.is_action_just_pressed("accept"):
-		if board.checkForOverlap(convertTetrominoToArray()):
-			board.lockTetrominoToBoard(convertTetrominoToArray(),get_node("Tetromino").shape)
-			createNewTetromino()
+	if Input.is_action_just_pressed("drop"):
+		dropTetromino()
 
+#Returns true if tetromino can still shift down, false if tetromino cannot shift down
 func shiftTetrominoDown():
-	if has_node("Tetromino"):
+	if has_node("Tetromino") and state == "Running":
 		var tetromino_can_move = true
 		var tetromino_locked = false
 		for i in convertTetrominoToArray():
 			var last_row = [230,231,232,233,234,235,236,237,238,239]
 			if last_row.has(int(i)) and !tetromino_locked:
 				board.lockTetrominoToBoard(convertTetrominoToArray(),get_node("Tetromino").shape)
+				board.checkForGameOver()
 				createNewTetromino()
 				tetromino_can_move = false
 				tetromino_locked = true
+				return false
 		if tetromino_can_move:
 			get_node("Tetromino").position.y += 32
 			if !board.checkForOverlap(convertTetrominoToArray()):
 				get_node("Tetromino").position.y -= 32
 				board.lockTetrominoToBoard(convertTetrominoToArray(),get_node("Tetromino").shape)
+				board.checkForGameOver()
 				createNewTetromino()
+				return false
+	return true
 
+#Recurs shiftTetrominoDown() to bring the tetromino to its lowest possible position
+func dropTetromino():
+	var can_shift_down = true
+	while can_shift_down:
+		can_shift_down = shiftTetrominoDown()
+
+#Frees current tetromino and creates a new tetromino
 func createNewTetromino():
 	if has_node("Tetromino"):
 		get_node("Tetromino").name = "OldTetromino"
-		get_node("OldTetromino").queue_free()
+		get_node("OldTetromino").reparent(cleanup)
+		cleanup_timer.start()
 	var new_tetromino = tetromino.instantiate()
 	add_child(new_tetromino)
 	new_tetromino.name = "Tetromino"
 	new_tetromino.position = Vector2(board_width/2,32)
-	#new_tetromino.initTetromino("I")
 	new_tetromino.initTetromino(Globals.Tetromino.keys()[randi() % Globals.Tetromino.size()])
 	movement_timer.start()
 
-func _on_movement_timer_timeout():
-	shiftTetrominoDown()
-
+#Converts tetromino into an array of 4 integers corresponding to locations in the board array
 func convertTetrominoToArray():
 	if has_node("Tetromino"):
 		var termino_position = get_node("Tetromino").position / 32
@@ -163,3 +228,14 @@ func convertTetrominoToArray():
 				square_position = termino_position + floor(square.position / 32)
 			array_to_return.append(square_position.x + square_position.y*10 + 40)
 		return array_to_return
+
+func initCleanup():
+	for child in cleanup.get_children():
+		child.visible = false
+		child.free()
+
+func _on_movement_timer_timeout():
+	shiftTetrominoDown()
+
+func _on_cleanup_timer_timeout():
+	initCleanup()
